1e5b1c189a796e6cba0a386236cfbc76e3cae2b9
Marko Prelevikj <5265944+pr3mar@users.noreply.github.com>
2019-05-12 15:32:16 +0200

    Revert "Indexer script and code organization"

--- indexer/db/__init__.py
+++
@@ -1,7 +0,0 @@
-import sqlite3
-
-
-# TODO handle sqlite
-class DB(object):
-    def __init__(self):
-        pass
--- indexer/preprocess/__init__.py
+++
@@ -1,9 +0,0 @@
-from utils import timing
-
-
-class Preprocess:
-    @staticmethod
-    @timing
-    def preprocess(inputPath, outputPath):
-        # TODO: @Stefan copy the code here
-        pass
