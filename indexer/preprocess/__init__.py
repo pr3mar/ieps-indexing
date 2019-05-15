d5ab40cddc9dad8352095d8077a98113a9582846
IvIUp <stefan.ivan.ivanisevic@gmail.com>
2019-05-15 22:26:55 +0200

    creating json works nice now

--- indexer/preprocess/__init__.py
+++ indexer/preprocess/__init__.py
@@ -7,12 +7,14 @@ from nltk.tokenize import word_tokenize
 from inscriptis import get_text
 from .stopwords import stop_words_slovene
 
-
+specialChars = ['!', '"', '""', "#", '$', '%', '&', '\'', '\'\'', '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', '\\', ']', '^', '_', '`', '``', '{', '|', '}', '~' ]
+    
 class Preprocess:
 
+    
     @staticmethod
     def tokenize(text):
-        return [token for token in word_tokenize(text) if token not in stop_words_slovene]
+        return [token for token in word_tokenize(text) if token not in stop_words_slovene and token not in specialChars]
 
     @staticmethod
     def contentize(text):
@@ -26,23 +28,27 @@ class Preprocess:
             with open(outputFilePath, "r") as file:
                 return json.load(file)
         processed = []
+        content = []
+        tokens = []
+        fileName = []
         # Find all .html files
         inputPaths = [f for f in glob.glob(rootInputPath + "**/*.html", recursive=True)]
         for idx, documentPath in enumerate(inputPaths):
             print(f"[{(idx / len(inputPaths)) * 100:.0f}%] Working on {documentPath}")
             with open(documentPath, 'r') as file:
                 text = re.sub(r'<[^<]+?>', '', get_text(file.read().lower()))
-                content = Preprocess.contentize(text)
-                tokens = Preprocess.tokenize(text)
-                processed[re.compile(r'.*/(.*).html').search(documentPath).group(1)] = {
-                    "tokens": tokens,
-                    "content": content
-                }
+                content.append(Preprocess.contentize(text))
+                tokens.append(Preprocess.tokenize(text))
+                fileName.append(re.compile(r'.*/(.*).html').search(documentPath).group(1))
+            
+        processed = {
+            filer: 
+            {
+                "tokens": tokens[i],
+                "content": content[i]
+            }for i, filer in enumerate(fileName)
+        }
+        
         with open(outputFilePath, "w") as file:
             json.dump(processed, file, indent=4)
         return processed
-
-
-# inputF = '/home/stefan/4th semester/Web Information Extraction And Retrieval/ieps-indexing/input'
-# outputF = '/home/stefan/4th semester/Web Information Extraction And Retrieval/ieps-indexing/output'
-# Preprocess.preprocessFiles(inputF, outputF)
