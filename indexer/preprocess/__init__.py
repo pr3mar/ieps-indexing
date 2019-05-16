13750c51d0ce87c3163d853ba65f249d1d8a32e2
Marko Prelevikj <pr3mar@users.noreply.github.com>
2019-05-16 13:56:23 +0200

    fixing output

--- indexer/preprocess/__init__.py
+++ indexer/preprocess/__init__.py
@@ -2,19 +2,17 @@ import glob
 import re
 import json
 import os
+import codecs
 from utils import timing
 from nltk.tokenize import word_tokenize
 from inscriptis import get_text
 from .stopwords import stop_words_slovene
 
-specialChars = ['!', '"', '""', "#", '$', '%', '&', '\'', '\'\'', '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', '\\', ']', '^', '_', '`', '``', '{', '|', '}', '~' ]
-    
 class Preprocess:
 
-    
     @staticmethod
     def tokenize(text):
-        return [token for token in word_tokenize(text) if token not in stop_words_slovene and token not in specialChars]
+        return [token for token in word_tokenize(text) if token not in stop_words_slovene and re.match('\w', token)]
 
     @staticmethod
     def contentize(text):
@@ -24,31 +22,20 @@ class Preprocess:
     @timing
     def preprocessFiles(rootInputPath, outputPath, forceUpdate=True):
         outputFilePath = f"{outputPath}/processed.json"
-        if os.path.isfile(outputFilePath) and forceUpdate == False:
+        if os.path.isfile(outputFilePath) and forceUpdate is False:
             with open(outputFilePath, "r") as file:
                 return json.load(file)
-        processed = []
-        content = []
-        tokens = []
-        fileName = []
-        # Find all .html files
+        processed = {}
         inputPaths = [f for f in glob.glob(rootInputPath + "**/*.html", recursive=True)]
         for idx, documentPath in enumerate(inputPaths):
             print(f"[{(idx / len(inputPaths)) * 100:.0f}%] Working on {documentPath}")
-            with open(documentPath, 'r') as file:
+            with open(documentPath, mode='r', encoding="utf-8") as file:
                 text = re.sub(r'<[^<]+?>', '', get_text(file.read().lower()))
-                content.append(Preprocess.contentize(text))
-                tokens.append(Preprocess.tokenize(text))
-                fileName.append(re.compile(r'.*/(.*).html').search(documentPath).group(1))
-            
-        processed = {
-            filer: 
-            {
-                "tokens": tokens[i],
-                "content": content[i]
-            }for i, filer in enumerate(fileName)
-        }
-        
-        with open(outputFilePath, "w") as file:
-            json.dump(processed, file, indent=4)
+                processed[re.compile(r'.*/(.*).html').search(documentPath).group(1)] = {
+                    "tokens": Preprocess.tokenize(text),
+                    "content": Preprocess.contentize(text)
+                }
+            break
+        with open(outputFilePath, mode="w", encoding="utf-8") as file:
+            json.dump(processed, file, ensure_ascii=False)
         return processed
